d8b2ca8b053a6865d7559576f92e2bf14d3d95d6
mperusso <matheus-perusso@hotmail.com>
2026-01-05 12:21:32 -0300

    feat: [pessoa] initial logic

--- src/main/java/com/dev/gth_test/controller/PessoaController.java
+++ src/main/java/com/dev/gth_test/controller/PessoaController.java
@@ -44,5 +44,7 @@ public class PessoaController {
         pessoaService.deletePessoa(id);
     }
 
+    //TODO: calcular massa
+
 
 }
--- src/main/java/com/dev/gth_test/task/Task.java
+++ src/main/java/com/dev/gth_test/task/Task.java
@@ -1,8 +1,11 @@
 package com.dev.gth_test.task;
 
+import com.dev.gth_test.entity.Pessoa;
 import com.dev.gth_test.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Task {
@@ -13,4 +16,42 @@ public class Task {
     public Task(PessoaRepository pessoaRepository) {
         this.pessoaRepository = pessoaRepository;
     }
+
+    public Pessoa createPessoa(Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+
+    public Pessoa updatePessoa(Long id, Pessoa pessoa) {
+        Pessoa existingPessoa = pessoaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa not found with id " + id));
+
+        existingPessoa.setNome(pessoa.getNome());
+        existingPessoa.setDataNasc(pessoa.getDataNasc());
+        existingPessoa.setCpf(pessoa.getCpf());
+        existingPessoa.setSexo(pessoa.getSexo());
+        existingPessoa.setAltura(pessoa.getAltura());
+        existingPessoa.setPeso(pessoa.getPeso());
+
+        return pessoaRepository.save(existingPessoa);
+    }
+
+    public void deletePessoa(Long id) {
+        Pessoa existingPessoa = pessoaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa not found with id " + id));
+        pessoaRepository.delete(existingPessoa);
+    }
+
+    public List<Pessoa> getAllPessoas() {
+        return pessoaRepository.findAll();
+    }
+
+
+
+
+
+
+
+
+
+
 }
