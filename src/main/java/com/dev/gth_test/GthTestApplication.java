84ea73a9a3502102d3b889f49a2b2edb671e5f40
mperusso <matheus-perusso@hotmail.com>
2026-01-05 10:31:53 -0300

    Initial commit

---
+++ src/main/java/com/dev/gth_test/GthTestApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gth_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GthTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/gth_test/GthTestApplicationTests.java
@@ -0,0 +1,9 @@
+package com.dev.gth_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GthTestApplicationTests {
+
+}
